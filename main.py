3edb0cad52fca9c1b054e0e7c5197b3712a3342a
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-03 20:42:41 +0200

    test

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+I'm only Sleeping
+"""
 import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
 
-import antigravity
+print(torch.__version__)
+a = np.linspace(0,10,1000)
+b = np.cos(a)
+
+plt.plot(a,b)
+plt.set_title('riciu')
+plt.show()
