5effba8ef4b207b96236e087bd96d2a6b1c2d61d
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-10 20:15:15 +0200

    working file

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import gym
-
 import numpy as np
 
 
 env = gym.make("MountainCar-v0")
 env.reset()
 
-for i in range(10):
-    print(env.action_space.sample( ))
+env_high = env.observation_space.high
+env_low = env.observation_space.low
+
+N_grid = 10
+
+grid_size = [N_grid]*len(env_high)
+discrete_size  = (env_high - env_low)/grid_size
+discrete_size
+
+
+done = False
+while not done:
+    # TODO
+    # - come scegliere l'azione
+    # - esecuzione dell'azione
+    # - calcolo ed update reward
+    #print(env.action_space.sample( ))
+    state, reward, done, _ = env.step(2)
     env.render()
-    #env.step(2)
 
 env.close()
