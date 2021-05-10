e925fdcf4e0a9bf614b6c14656bb49a37f40976f
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-10 21:43:31 +0200

    Add for-loop episodes

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import numpy as np
 #Hyper parameters
 learning_rate = 0.1
 discount_f = 0.90
+episodes = 10000
+n_show = 1000
 
+# Loading env
 env = gym.make("MountainCar-v0")
 env.reset()
 
 # Dimension of the environment
 env_high = env.observation_space.high
 env_low = env.observation_space.low
+env_goal = env.goal_position
+
 
 # Discretisation
-N_grid = 10
+N_grid = 1000
 grid_size = [N_grid]*len(env_high)
 n_actions = env.action_space.n
 discrete_size  = (env_high - env_low)/grid_size
@@ -29,27 +34,35 @@ def discretize(state):
 
 q_table = np.random.uniform(low=-2, high=0, size=(grid_size + [n_actions]))
 
-q_table.shape
-# Training
-discrete_state = discretize(env.reset())
 
-done = False
-while not done:
-    # TODO
-    # - come scegliere l'azione
-    # - esecuzione dell'azione
-    # - calcolo ed update reward
-    #print(env.action_space.sample( ))
-    action = np.argmax(q_table[discrete_state])
-    new_state, reward, done, _ = env.step(action)
-    new_discrete_state = discretize(new_state)
-    if not done:
-        max_next_q = np.max(q_table[new_discrete_state])
-        current_q = q_table[discrete_state + (action,)]
-        new_q_value = (1-learning_rate)*current_q + learning_rate*(reward + discount_f*max_next_q)
-        q_table[new_discrete_state + (action,)] = new_q_value
+for episode in range(episodes):
+    # Training
+    discrete_state = discretize(env.reset())
+    if episode % n_show == 0:
+        to_render = True
+    else:
+        to_render = False
+    done = False
+    while not done:
+        # TODO
+        # - come scegliere l'azione
+        # - esecuzione dell'azione
+        # - calcolo ed update reward
+        #print(env.action_space.sample( ))
+        action = np.argmax(q_table[discrete_state])
+        new_state, reward, done, _ = env.step(action)
+        new_discrete_state = discretize(new_state)
+        if not done:
+            max_next_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state + (action,)]
+            new_q_value = (1-learning_rate)*current_q + learning_rate*(reward + discount_f*max_next_q)
+            q_table[new_discrete_state + (action,)] = new_q_value
 
-    discrete_state = new_discrete_state
-    env.render()
+        elif new_state[0] >= env_goal:
+            q_table[new_discrete_state + (action, )] = 0
+            print("Goal reached, MOFOS")
+        discrete_state = new_discrete_state
+        if to_render:
+            env.render()
 
 env.close()
