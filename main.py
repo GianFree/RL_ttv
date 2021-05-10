efda50327e67a662837ed753d5721f243314ccd0
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-10 19:52:49 +0200

    dummy loop

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ env = gym.make("MountainCar-v0")
 env.reset()
 
 for i in range(10):
-    print(env.action_space.sample())
-    #env.render()
+    print(env.action_space.sample( ))
+    env.render()
     #env.step(2)
 
 env.close()
