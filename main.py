1fad14b6d87e4e33a79c1253a9779bfbf0beadfb
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-10 21:22:35 +0200

    working episode

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import gym
 import numpy as np
 
+#Hyper parameters
+learning_rate = 0.1
+discount_f = 0.90
 
 env = gym.make("MountainCar-v0")
 env.reset()
@@ -12,13 +15,24 @@ env_low = env.observation_space.low
 # Discretisation
 N_grid = 10
 grid_size = [N_grid]*len(env_high)
-n_actions = [env.action_space.n]
+n_actions = env.action_space.n
 discrete_size  = (env_high - env_low)/grid_size
 
+def discretize(state):
+    """
+    Input: state ("continous")
+    Output: discrete_state ( in the grid )
+    """
+    discrete_state = (state - env_low)/discrete_size
+    return tuple(discrete_state.astype(np.int))
 
-q_table = -np.ones((grid_size + [n_actions]), float)
+
+q_table = np.random.uniform(low=-2, high=0, size=(grid_size + [n_actions]))
+
 q_table.shape
 # Training
+discrete_state = discretize(env.reset())
+
 done = False
 while not done:
     # TODO
@@ -26,8 +40,16 @@ while not done:
     # - esecuzione dell'azione
     # - calcolo ed update reward
     #print(env.action_space.sample( ))
-    state, reward, done, _ = env.step(0)
-    print(state, reward, done)
+    action = np.argmax(q_table[discrete_state])
+    new_state, reward, done, _ = env.step(action)
+    new_discrete_state = discretize(new_state)
+    if not done:
+        max_next_q = np.max(q_table[new_discrete_state])
+        current_q = q_table[discrete_state + (action,)]
+        new_q_value = (1-learning_rate)*current_q + learning_rate*(reward + discount_f*max_next_q)
+        q_table[new_discrete_state + (action,)] = new_q_value
+
+    discrete_state = new_discrete_state
     env.render()
 
 env.close()
