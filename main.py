69b83375a8d2a449ee9180e3fef6d9f134fb2203
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-10 20:26:33 +0200

    qtable up

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import numpy as np
 env = gym.make("MountainCar-v0")
 env.reset()
 
+# Dimension of the environment
 env_high = env.observation_space.high
 env_low = env.observation_space.low
 
+# Discretisation
 N_grid = 10
-
 grid_size = [N_grid]*len(env_high)
+n_actions = [env.action_space.n]
 discrete_size  = (env_high - env_low)/grid_size
-discrete_size
 
 
+q_table = -np.ones((grid_size + [n_actions]), float)
+q_table.shape
+# Training
 done = False
 while not done:
     # TODO
@@ -22,7 +26,8 @@ while not done:
     # - esecuzione dell'azione
     # - calcolo ed update reward
     #print(env.action_space.sample( ))
-    state, reward, done, _ = env.step(2)
+    state, reward, done, _ = env.step(0)
+    print(state, reward, done)
     env.render()
 
 env.close()
