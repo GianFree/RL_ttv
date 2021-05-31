4f0f56dd66b5edcb7e7cc620a2954c75b7a2177a
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-31 20:46:15 +0200

    ANN initialized

---
+++ regression.py
@@ -0,0 +1,67 @@
+# Regression:
+# approximate an unknown function via Artificial Neural Networks.
+#
+# 1. Define the "unknown" function and generate data
+# 2. Define the ANN
+# 3. Training
+# 4. Test
+#
+# Possible upgrade:
+# - ANN for house pricing
+# - Comparison for stock pricing:
+#   - ANN
+#   - Long short-term memory
+#   - Bayesian approach to NN
+
+import torch
+import torch.utils.data as Data
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as fnc
+import torch.optim as optim
+import seaborn as sns
+import numpy as np
+
+
+# 1. Defining the input set (and allowing gradients)
+def f(x):
+    """Unknown function"""
+    return 2.5 * x
+
+
+x = torch.unsqueeze(torch.linspace(-2, 2,100, requires_grad=True),dim=1)
+y = f(x) + torch.rand(x.size())
+
+
+sns.scatterplot(x=x.data.numpy().squeeze(),y=y.data.numpy().squeeze())
+
+# 2. Defining the neural network
+class Regression(nn.Module):
+    def __init__(self):
+        super(Regression, self).__init__()
+        self.fc1 = nn.Linear(in_features=1,  out_features=20)
+        self.fc2 = nn.Linear(in_features=20, out_features=10)
+        self.fc3 = nn.Linear(in_features=10, out_features=1)
+
+
+    def forward(self, x):
+        x = fnc.relu(self.fc1(x))
+        x = fnc.relu(self.fc2(x))
+        x = self.fc3(x)
+
+regression_net = Regression()
+
+# Loss function - Do we need to specify a `reduction` as keyarg?
+loss_function = nn.MSELoss()
+# optimizer
+optimizer = optim.SGD(regression_net.parameters(),lr = 1e-3)
+
+# 3. Training the Network
+predictions = regression_net(x)
+loss = loss_function(predictions,y)
+
+optimizer.zero_grad()
+loss.backward()
+
+optimizer.step()
+print(loss.item())
