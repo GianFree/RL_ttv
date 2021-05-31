e9b7907679ed292051539ad3368dfe668a475de8
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-05-31 20:45:43 +0200

    all classification

--- classification_with_convolution.py
+++ classification_with_convolution.py
@@ -26,51 +26,138 @@
 # 4. test the model
 
 import torch
+from torch.utils.data import DataLoader
 from torchvision import datasets
 import seaborn as sns
-from torchvision.transforms import ToTensor, Lambda
+import torchvision
+from torchvision import transforms
 # to import layers
 import torch.nn as nn
 # to import activation functions
 import torch.nn.functional as fcn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
-train_set = datasets.FashionMNIST(root="data",download=True, transform=ToTensor())
-test_set = datasets.FashionMNIST(root="data",download=True,train=False)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using {} device'.format(device))
 
-train_data = train_set.data/255.0
-train_labels = train_set.targets
-test_data = test_set.data/255.0
-test_labels = test_set.targets
+transf_ = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=0.5, std=0.5)])
+
+batch_size = 8
+train_set = datasets.FashionMNIST(root="data",download=True, transform=transf_)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+test_set = datasets.FashionMNIST(root="data",download=True,train=False, transform=transf_)
+test_loader = DataLoader(test_set, batch_size=batch_size)
 
 
-sns.heatmap(train_data[1], square=True)
-print(train_labels.unique())
-
-
+#datait_ = iter(train_loader)
+#images, labels = datait_.next()
+#plt.imshow(images.squeeze())
+#sns.heatmap(images.squeeze(), square=True)
 
 # creaton of the model:
 class BUBU(nn.Module):
     def __init__(self):
         super(BUBU, self).__init__()
-        self.conv1 = nn.Conv2d(1,16,3)
+        # processing 2d image
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=16,kernel_size=2, padding=1)
         self.pool = nn.MaxPool2d((2,2))
-        self.conv2 = nn.Conv2d(16,32,(3,3))
+        self.conv2 = nn.Conv2d(in_channels=16,out_channels=32,kernel_size=2, padding=1)
         self.pool = nn.MaxPool2d((2,2))
-
-        #self.fc1 = nn.Linear()
-        #self.fc2 = nn.Softmax(10)
+        # linear layers
+        self.fc1 = nn.Linear(in_features=7*7*32, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=128)
+        self.fc3 = nn.Linear(in_features=128, out_features=64)
+        self.fc4 = nn.Linear(in_features=64,  out_features=10)
+        #self.fc5 = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.pool(fcn.relu(self.conv1(x)))
         x = self.pool(fcn.relu(self.conv2(x)))
         x = torch.flatten(x,1)
-        #x = fcn.relu(self.fc1(x))
-        #x = self.fc2(x)
+        x = fcn.relu(self.fc1(x))
+        x = fcn.relu(self.fc2(x))
+        x = fcn.relu(self.fc3(x))
+        x = fcn.relu(self.fc4(x))
+        #x = self.fc5(x)
         return x
 
 
 net = BUBU()
 print(net)
-gneggne = train_set.data.unsqueeze(1)
-net(gneggne)
+
+loss_ = nn.CrossEntropyLoss()
+#opt_  = optim.SGD(net.parameters(),lr = 1e-3,momentum = 0.9)
+opt_  = optim.Adam(net.parameters(),lr = 1e-3)
+
+
+
+### Training loop
+def training_routine(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X,y) in enumerate(dataloader):
+        #prediction
+        pred = model(X)
+        loss = loss_fn(pred,y)
+
+        #backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 1000 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+    return loss
+
+
+def test_routine(test_dataloader, model, loss_fn):
+    size = len(test_dataloader.dataset)
+    test_loss, correct = 0,0
+
+    with torch.no_grad():
+        for X,y in test_dataloader:
+            # prediction
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= size
+    correct /= size
+
+    print(f"Test Error:\n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return correct*100
+
+
+n_epochs = 30
+test_losses = []
+train_losses = []
+for t in range(n_epochs):
+    print(f"Epoch {t+1}\n")
+    training_routine(train_loader, net, loss_, opt_)
+    test_losses.append(test_routine(test_loader, net, loss_))
+
+print("Done!")
+
+torch.save(net.state_dict(), "adam_fashionMnist_statedict.pth")
+#device = torch.device('cpu')
+#model = TheModelClass(*args, **kwargs)
+#model.load_state_dict(torch.load(PATH, map_location=device))
+# or
+#device = torch.device("cuda")
+#model = TheModelClass(*args, **kwargs)
+#model.load_state_dict(torch.load(PATH))
+#model.to(device)
+torch.save(net, "adam_fashionMnist_model.pth")
+# Model class must be defined somewhere
+#model = torch.load(PATH)
+#model.eval()
+
+#plt.plot(train_losses, label="training set")
+plt.plot(test_losses, label="test set")
+plt.legend(loc='best')
+plt.show()
