96d67fe13b9363183915dd323d31a2b3a3487228
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2021-10-04 21:47:46 +0200

    testing elon

---
+++ Elon_Suka.py
@@ -0,0 +1,152 @@
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from collections import namedtuple, deque
+from itertools import count
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+
+
+# Reinforcement learning using Lunar LunarLander.
+env = gym.make("LunarLander-v2").unwrapped
+
+# 1. Investigate the environment
+observation = env.reset()
+actions = env.action_space
+
+
+env.render()
+"""
+print(observation)
+
+Actions:  Nop, fire left engine, main engine, right engine
+
+
+env: The environment
+        s (list): The state. Attributes:
+                  s[0] is the horizontal coordinate
+                  s[1] is the vertical coordinate
+                  s[2] is the horizontal speed
+                  s[3] is the vertical speed
+                  s[4] is the angle
+                  s[5] is the angular speed
+                  s[6] 1 if first leg has contact, else 0
+                  s[7] 1 if second leg has contact, else 0
+"""
+'''
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
+'''
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# Wanna use GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+## Replay Memory
+Transition  = namedtuple('Transition',('state','action','next_state','reward'))
+
+
+class ReplayMemory(object):
+    """
+    Stores the information """
+    def __init__(self,capacity):
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+         return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+# 2. Creating the neural network
+
+# Neural network
+class DQN(nn.Module):
+    def __init__(self, h, w, outputs):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+
+        # We apply 3 time the cnn, and we scale 3 times the images
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x, debug=False):
+        x = x.to(device)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        if debug:
+            print(f"the size of the vector is {x.size()}")
+        # it is a bit unclear the output of x.view()
+        return self.head(x.view(x.size(0), -1))
+
+resize = T.Compose([T.ToPILImage(), T.Resize(40, interpolation=Image.CUBIC), T.ToTensor()])
+
+"""
+def get_lander_location(screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+"""
+
+def get_screen():
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    if screen.shape[1:] != (400,600):
+        # Cart is in the lower half, so strip off the top and bottom of the screen
+        _, screen_height, screen_width = screen.shape
+        screen = screen[:, int(screen_height*0.5):int(screen_height * 0.5)]
+    print(f"Screen dimensions: {screen.shape}")
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0)
+
+
+# TODO:
+#
+# 3. training the nn
+# 4. plot loss function with respect to epochs
